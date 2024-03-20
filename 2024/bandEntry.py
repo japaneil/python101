5b245519f44feb3ddac397be2465027deaecda30
Neil Arora <neilarora2008@gmail.com>
2024-03-21 03:01:30 +0530

    added comments

--- 2024/bandEntry.py
+++ 2024/bandEntry.py
@@ -1,8 +1,14 @@
+# Get name for first member
 Name1 = input("Please enter details of member 1:\nName: ")
+# Get instrument for first member
 Ins1 = input("Please enter details of member 1:\nInstrument: ")
+# Get name for second member
 Name2 = input("Please enter details of member 2:\nName: ")
+# Get instrument for second member
 Ins2 = input("Please enter details of member 2:\nInstrument: ")
+# Get name for third member (if present)
 Name3 = input("Please enter details of member 3:\nName: ")
+# Get instrument for third member
 Ins3 = input("Please enter details of member 3:\nInstrument: ")
 
 mem1 = {
@@ -24,7 +30,7 @@ band = {
     "Member 3": mem3
 }
 
-print(band)
+print(band)  # Print out the band information
 print("\nVariety of instruments are " +
       band["Member 1"]["Instrument"] + ", " +
       band["Member 2"]["Instrument"] + " and " +
