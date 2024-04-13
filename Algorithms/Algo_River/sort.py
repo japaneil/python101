7141276c0ba1a0b969151937e198868210ed7c2b
Neil Arora <neilarora2008@gmail.com>
2024-04-13 20:00:09 +0530

    edit: changed functio arguments

--- Algorithms/Algo_River/sort.py
+++ Algorithms/Algo_River/sort.py
@@ -5,8 +5,8 @@ matrix = [
     [0,1,0,1]
 ]
 tnum = 0
-def num(x: int):
-    tnum = (matrix[i].count(x))
+def num(x,y):
+    tnum = (matrix[y].count(x))
     return tnum
 l_coord = []
 
@@ -16,17 +16,18 @@ for i in range(len(matrix)):
             j = matrix[i].index(1)
             l_coord.append(tuple([i,j]))
             cnum +=1
-            while cnum <= num(1):
+            while cnum <= num(1,i):
                 k = matrix[i].index(1,j+x,len(matrix[i]))
                 l_coord.append(tuple([i,k]))
                 cnum +=1
-                if cnum != num(1):
+                if cnum != num(1,i):
                     x += 1
-                    print(cnum == num(1))
+                    print(cnum == num(1,i))
                     print(x)
                 else:
                     x = 0
-                    print(cnum == num(1))
+                    print(cnum == num(1,i))
+                    print(x)
 s_coord = set(l_coord)    
 print(l_coord)        
 print(s_coord)
