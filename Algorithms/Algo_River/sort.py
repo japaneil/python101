11e20d15e438db5090601d352a391a95cea35418
Neil Arora <neilarora2008@gmail.com>
2024-04-13 19:40:54 +0530

    mod: changed skeleton

--- Algorithms/Algo_River/sort.py
+++ Algorithms/Algo_River/sort.py
@@ -27,5 +27,6 @@ for i in range(len(matrix)):
                 else:
                     x = 0
                     print(cnum == num(1))
-s_coord = set(l_coord)            
+s_coord = set(l_coord)    
+print(l_coord)        
 print(s_coord)
